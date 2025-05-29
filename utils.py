aa5f745314f62d50aedccb5c8c40a89681c74253
Duc An <an.nguyenhoduc@hcmut.edu.vn>
2025-05-29 12:56:19 +0700

    feat: get wandb api key from multiple source and skip if none

--- utils.py
+++ utils.py
@@ -1,16 +1,33 @@
-from kaggle_secrets import UserSecretsClient
+import os
 import wandb
 
 
-def wandb_init(lr0, epochs, batch_size):
-    wandb_key = UserSecretsClient().get_secret("WANDB_API_KEY")
-    wandb.login(key=wandb_key)
+def wandb_init(lr0, epochs, batch_size, project="mp242"):
+    api_key = os.getenv("WANDB_API_KEY", None)
+
+    if api_key is None:
+        try:
+            from kaggle_secrets import UserSecretsClient
+
+            api_key = UserSecretsClient().get_secret("WANDB_API_KEY")
+        except Exception:
+            api_key = None
+
+    # Skip if no api_key
+    if not api_key:
+        print("WANDB_API_KEY not found; skipping wandb.init()")
+        return None
+
+    # os.environ["WANDB_API_KEY"] = api_key
+    wandb.login(key=api_key)
+
     logger = wandb.init(
-        project="mp242",
+        project=project,
         config={"lr0": lr0, "epochs": epochs, "batch_size": batch_size},
     )
     logger.define_metric("eval/precision", summary="max")
     logger.define_metric("eval/recall", summary="max")
     logger.define_metric("eval/mAP50", summary="max")
     logger.define_metric("eval/mAP5095", summary="max")
+
     return logger
