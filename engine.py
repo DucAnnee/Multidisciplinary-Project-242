bbcfaf1a70b35adf482d021afffe74adf5c6b8b2
Duc An <an.nguyenhoduc@hcmut.edu.vn>
2025-05-29 11:49:22 +0700

    feat: add gradient clip

--- engine.py
+++ engine.py
@@ -22,6 +22,7 @@ def train_one_epoch(
 
     # Training
     model.train()
+    optimizer.zero_grad()
     torch.cuda.empty_cache()
     accu_loss = 0.0
     if rank == 0:
@@ -36,8 +37,8 @@ def train_one_epoch(
         loss_dict = model(images, targets)
         loss = sum(loss for loss in loss_dict.values())
 
-        optimizer.zero_grad()
         loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
         optimizer.step()
 
         accu_loss += loss.item()
