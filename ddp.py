becc3088649a50a596d6567fc684b6e2517fbaf1
Duc An <an.nguyenhoduc@hcmut.edu.vn>
2025-05-29 10:40:51 +0700

    feat: remove dynamic port finding, use static port 12355

--- ddp.py
+++ ddp.py
@@ -3,17 +3,9 @@ import torch
 import torch.distributed as dist
 
 
-def find_free_port():
-    import socket
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(("127.0.0.1", 0))
-        return s.getsockname()[1]
-
-
 def ddp_init(rank, world_size):
     os.environ["MASTER_ADDR"] = "localhost"
-    os.environ["MASTER_PORT"] = str(find_free_port())
+    os.environ["MASTER_PORT"] = "12355"
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
     torch.cuda.set_device(rank)
     device = torch.device(f"cuda:{rank}")
