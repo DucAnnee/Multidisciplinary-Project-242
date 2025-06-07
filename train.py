36cefaaca104025a7d6ea7587771ff5adf96fee4
Duc An <an.nguyenhoduc@hcmut.edu.vn>
2025-06-08 06:38:11 +0700

    fix: missing paramater

--- train.py
+++ train.py
@@ -148,6 +148,7 @@ def main_worker(rank, world_size, args):
                 rank,
                 epoch,
                 args.epochs,
+                det_metrics,
                 logger,
             )
             mAP5095 = stats["eval/mAP5095"]
